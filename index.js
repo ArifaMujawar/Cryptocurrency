4a2d3119dda40480e39d14e3e7cf1bb485f54849
ArifaMujawar <arifabegam.mujawar@gmail.com>
2018-06-04 18:40:50 +0300

    Added basic functionalities

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let data = "";
+
+let obj = "";
 onLoad();
 
 // displayData();
@@ -6,13 +7,10 @@ onLoad();
 
 function displayData(data){
     let count = 1;
-    const table = document.getElementById('table');
+    let table = document.getElementById('table');
     for(let i = 0; i < data.length; i++){
-       
-        // console.log(data[i]);
         helper(data[i],table, "name", "price_usd", "rank", "last_updated",count++);
     }
-    
 }
 
 function helper(value, table, name, price, rank, change, count){
@@ -35,9 +33,13 @@ function helper(value, table, name, price, rank, change, count){
     tr.appendChild(td_rank);
     tr.appendChild(td_change);
 
-    table.appendChild(tr);
-}
 
+   table.appendChild(tr);
+}
+const refreshTable = () => {
+  const table = document.getElementById('table');
+    let x = document.querySelectorAll(".rows").forEach(e => e.parentNode.removeChild(e));
+}
 
 function onLoad(){
     const url = "https://api.coinmarketcap.com/v1/ticker/?limit=10";
@@ -49,25 +51,74 @@ function onLoad(){
         data = myJson;
     // console.log(data);
     displayData(data);
+    AddData(data);
   });
 }
-showDropDown = () =>{
-    document.getElementById("myDropdown").classList.toggle("show");
+
+const AddData = (data) => {
+  console.log('adding data');
+  obj = data;
+  console.log(obj);
 }
 
-sortBy = (e) => {
-console.log("check ",e);
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-  
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+
+  function handleSort(){
+    const selectedID = document.getElementById('sort').selectedIndex;
+    console.log(selectedID);
+    if(selectedID == "0"){
+      console.log('selected name sort');
+      sortByName();
     }
+    else if(selectedID == "1"){
+      console.log('selected price sort');
+      sortByPriceAsc();
+    }
+    else if(selectedID == "2"){
+      console.log('selected rank sort');
+      sortByRank();
+    }
+  }
+  function sortByPriceAsc(){
+   refreshTable();
+   obj.sort(function(a, b){
+    return a.price_usd - b.price_usd;    
+   });
+   displayData(obj);
+  console.log('after changing by price',obj);
+  }
+
+  
+  
+  function sortByRank(){
+   refreshTable();
+   obj.sort(function(a, b){
+      return a.rank - b.rank;
+   });
+   displayData(obj);
+
+  }
+
+  function sortByName(){
+    refreshTable();
+    obj.sort(function(a, b){
+      if(a.name.toLowerCase() < b.name.toLowerCase()){
+        return -1;
+      }else if(b.name.toLowerCase() < a.name.toLowerCase()){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+  displayData(obj);
+  };
+
+  function filterCoins(){
+    const inputText = document.getElementById('search').value;
+    console.log(inputText);
+    const filteredCoins = obj.filter(function(coin){
+    return coin.name.toLowerCase().includes(inputText.toLocaleLowerCase());
+    });
+    console.log(filteredCoins);
+    refreshTable();
+    displayData(filteredCoins);
   }
